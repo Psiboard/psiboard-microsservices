a4bb4e1eb13e3db193d2e6158f596f74d51351f2
NogueiraDan <silvanogueira3460@gmail.com>
2024-09-12 19:32:49 -0300

    feat: Scaffold patients resource

--- client-bff/src/app.module.ts
+++ client-bff/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './app/modules/users-serivce/users.module';
+import { PatientsModule } from './app/modules/patients-service/patients.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PatientsModule],
   controllers: [],
   providers: [],
 })
