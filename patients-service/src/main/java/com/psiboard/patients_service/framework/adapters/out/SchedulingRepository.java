989c1c65f137ba6fc0eba967c4a5005eb641ddb4
NogueiraDan <silvanogueira3460@gmail.com>
2024-09-29 17:11:05 -0300

    feat: implements available schdules query derivation

--- patients-service/src/main/java/com/psiboard/patients_service/framework/adapters/out/SchedulingRepository.java
+++ patients-service/src/main/java/com/psiboard/patients_service/framework/adapters/out/SchedulingRepository.java
@@ -2,7 +2,9 @@ package com.psiboard.patients_service.framework.adapters.out;
 
 import java.time.LocalDate;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.psiboard.patients_service.domain.Scheduling;
 
@@ -13,4 +15,6 @@ public interface SchedulingRepository extends JpaRepository<Scheduling, String> 
 
     // Verifica se existe um agendamento em uma data e hora específicas
     boolean existsByDateAndHour(LocalDate date, String hour);
+
+    List<Scheduling> findByDate(LocalDate date);
 }
